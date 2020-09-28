37d0c5e3e86e9bc4a0b8836fdd59fc1295bc06ae
anagvillanueva <anafernandagv27@gmail.com>
2020-09-27 21:38:07 -0500

    Cambio #2

---
+++ pokeapi/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/index.scss'
+
+function App() {
+  return (
+  <>
+    <nav>
+      <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"alt="PokeApi"
+      className='logo'
+      />
+    </nav>
+  </>
+  )
+  
+}
+
+export default App;
