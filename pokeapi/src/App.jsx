063a693035ef493b188ac193aefdfd4549111f1e
Bruno Cruz Granados <indiebrus@gmail.com>
2020-09-27 22:27:05 -0500

    Estilizado de articles. Cambio #3

--- pokeapi/src/App.jsx
+++ pokeapi/src/App.jsx
@@ -5,10 +5,39 @@ function App() {
   return (
   <>
     <nav>
-      <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"alt="PokeApi"
-      className='logo'
+      <img 
+	src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+	alt="PokeApi"
+	className='logo'
       />
     </nav>
+    <main>
+      <section className='results'>
+	
+	<article>
+	  <header>	  
+	    <img
+	    src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+	    alt="Pokeball"/>
+	  </header>
+	  <footer>
+	    Pokemon!
+	  </footer>
+	</article>
+	
+	<article>
+	  <header>	  
+	    <img
+	    src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+	    alt="Pokeball"/>
+	  </header>
+	  <footer>
+	    Pokemon!
+	  </footer>
+	</article>
+
+      </section>
+    </main>
   </>
   )
   
